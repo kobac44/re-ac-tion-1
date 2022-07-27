8dcbea905d777cc8fef675e8f1e784104fbf1293
kobac44 <kobie.watkins@gmail.com>
2022-07-27 15:08:44 -0400

    added 3boxes for the drop and drag app

---
+++ src/components/boxes.js
@@ -0,0 +1,19 @@
+import './boxes.css';
+
+function Boxes() {
+    return (
+    <div className="container-1">
+        <div className="boxes-1">
+            <h3>Box #1</h3>
+        </div>
+        <div className="boxes-2">
+            <h3>Box #2</h3>
+        </div>
+        <div className="boxes-3">
+            <h3>Box #3</h3>
+        </div>
+    </div>
+    )
+}
+
+export default Boxes;
