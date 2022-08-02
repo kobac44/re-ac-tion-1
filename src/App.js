f5727bc471f873765a33e7071221ec75049d5a9c
kobac44 <kobie.watkins@gmail.com>
2022-08-01 22:58:48 -0400

    deleted react dnd and using vanilla JavaScript for react drag and drop function

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import { DndProvider } from 'react-dnd'
-import { HTML5Backend } from 'react-dnd-html5-backend'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <DndProvider backend={HTML5Backend}>
         <div className="content">
           <h1>Drop and Drag</h1>
         </div>
-      </DndProvider>
     </div>
   ); 
 }
--- src/components/boxes.js
+++ src/components/boxes.js
@@ -1,20 +1,33 @@
-// import   { useState, useRef} from 'react';
-// import { useDrag } from 'react-dnd'
+import React, { useState, useRef } from 'react';
 import './boxes.css';
 
 
 function Boxes() {
-    // const dragItem = useRef()
-    // const [list, setList] = useState(['Boxee-1', 'Boxee-2', 'Boxee-3']);
+    const dragItem = useRef();
+    const [list, setList] = useState(['Boxee 1','Boxee 2','Boxee 3','Boxee 4']);
 
+    const dragStart = (e, position) => {
+        dragItem.current = position;
+        console.log(e.target.innerHTML);
+    };
+    
 
 
     return (
-    <div className="item-boxee">
-        <div className="Boxee-1">Boxee #1</div>
-        <div className="Boxee-2">Boxee #2</div>
-        <div className="Boxee-3">Boxee #3</div>
-    </div>
+       {
+        list &&
+        list.map((boxee, index) => (
+            <div className="item-boxee">
+                <div className="Boxee-1">Boxee #1</div>
+                <div className="Boxee-2">Boxee #2</div>
+                <div className="Boxee-3">Boxee #3</div>
+                <div className="Boxee-4">Boxee #4</div>
+            onDragStart={(e) => dragStart(e, index)}
+            key={index}
+            draggable
+                {boxee}
+            </div>
+        )}
     )
 }
 
